90a349e85efdde61702e55f7208ce85316524341
JSN3 <jtk001@ucsd.edu>
2022-05-03 20:00:53 -0700

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,28 +9,79 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
+        /* find the next [, then find the ], then find the (, then read link
+            upto next) */
+
         int currentIndex = 0;
-        /*while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-        }*/
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+
+        // fix to test-file5.md "< and >"
+        if (markdown.indexOf("[") == -1 &&
+            markdown.indexOf("(") == -1) {
+            while(currentIndex < markdown.length()) {
+                int leftCarrotOne = markdown.indexOf("<", currentIndex);
+                int rightCarrotOne = markdown.indexOf(">",
+                    leftCarrotOne);
+                int leftCarrotTwo = markdown.indexOf("<", rightCarrotOne);
+                int rightCarrotTwo = markdown.indexOf(">", leftCarrotTwo);
+                toReturn.add(markdown.substring(leftCarrotTwo + 1,
+                    rightCarrotTwo));
+                currentIndex = rightCarrotTwo + 1;
+            }
         }
+
+        // fix to test-file2.md "[ and ]"
+        else if (markdown.indexOf("(") == -1 &&
+            markdown.indexOf("<") == -1) {
+            while(currentIndex < markdown.length()) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openBracketTwo = markdown.indexOf("[", closeBracket);
+                int closeBracketTwo = markdown.indexOf("]", openBracketTwo);
+                toReturn.add(markdown.substring(openBracketTwo + 1,
+                    closeBracketTwo));
+                currentIndex = closeBracketTwo + 1;
+            }
+        }
+
+        // fix to test-file4.md "( and )"
+        else if (markdown.indexOf("[") == -1 &&
+            markdown.indexOf("<") == -1) {
+            while(currentIndex < markdown.length()) {
+                int openParen = markdown.indexOf("(", currentIndex);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+        }
+
+        // fix to test-file3.md "() and []"
+        else if (markdown.indexOf("[") != -1 &&
+            markdown.indexOf("(") != -1 &&
+            markdown.indexOf("(") < markdown.indexOf("[")) {
+            while(currentIndex < markdown.length()) {
+                int openParen = markdown.indexOf("(", currentIndex);
+                int closeParen = markdown.indexOf(")", openParen);
+                int openBracket = markdown.indexOf("[", closeParen);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                toReturn.add(markdown.substring(openBracket + 1, closeBracket));
+                currentIndex = closeBracket + 1;
+            }
+        }
+        
+        // runs the default test-file.md
+        else {
+            while(currentIndex < markdown.length()) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+        }
+
         return toReturn;
     }
-
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,19 +8,61 @@ import java.io.IOException;
 
 public class MarkdownParseTest {
     
-@Test
-public void addition() {
-    assertEquals(2, 1 + 1);
-}
+    String winPath = "c:/Users/kimtj/Documents/GitHub/markdown-parser/";
 
-@Test
-public void getLinksTest() throws IOException {
-    Path fileName = Path.of("test-file.md");
-    String content = Files.readString(fileName);
-    ArrayList expected = new ArrayList<String>();
-    expected.add("https://something.com");
-    expected.add("some-thing.html");
-    assertEquals(expected, MarkdownParse.getLinks(content));
-}
+    @Test
+    public void addition() {
+        assertEquals(3, 1 + 1);
+    }
+
+    @Test
+    public void getLinksTest() throws IOException {
+        Path fileName = Path.of(winPath + "test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinksTestTwo() throws IOException {
+        Path fileName = Path.of(winPath + "test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinksTestThree() throws IOException {
+        Path fileName = Path.of(winPath + "test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinksTestFour() throws IOException {
+        Path fileName = Path.of(winPath + "test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinksTestFive() throws IOException {
+        Path fileName = Path.of(winPath + "test-file5.md");
+        String content = Files.readString(fileName);
+        ArrayList expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
 
 }
